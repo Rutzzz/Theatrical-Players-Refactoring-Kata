4f442e598c01986c207a48ea4912e808ec1788de
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-05-04 20:48:30 +0200

    added indented writer

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Globalization;
+using System.IO;
 using System.Text;
 using System.Threading;
 
@@ -13,7 +15,7 @@ namespace TheatricalPlayersRefactoringKata
         {
             var result = new StringBuilder($"Statement for {invoice.Customer}\n");
 
-            foreach(var (perf, amount) in invoice.Performances) 
+            foreach (var (perf, amount) in invoice.Performances)
             {
                 Play play = perf.Play;
                 result.AppendFormat(CultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(amount / 100), perf.Audience);
@@ -21,27 +23,38 @@ namespace TheatricalPlayersRefactoringKata
 
             result.AppendFormat(CultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(invoice.TotalAmount / 100));
             result.Append($"You earned {invoice.VolumeCredits} credits\n");
-            
+
             return result.ToString();
         }
-        
+
         public string PrintAsHtml(Invoice invoice)
         {
-            var result = new StringBuilder("<html>").AppendLine();
-            result.AppendLine($"  <h1>Statement for {invoice.Customer}</h1>");
-            result.AppendLine("  <table>");
-            result.AppendLine("    <tr><th>play</th><th>seats</th><th>cost</th></tr>");
+            var result = new StringBuilder();
 
-            foreach(var (perf, amount) in invoice.Performances) 
+            using var writer = new IndentedTextWriter(new StringWriter(result), "  ");
+
+            writer.WriteLine("<html>");
+            writer.Indent++;
+
+            writer.WriteLine($"<h1>Statement for {invoice.Customer}</h1>");
+            writer.WriteLine("<table>");
+
+            writer.Indent++;
+            writer.WriteLine("<tr><th>play</th><th>seats</th><th>cost</th></tr>");
+            
+            foreach (var (perf, amount) in invoice.Performances)
             {
                 Play play = perf.Play;
-                result.AppendFormat(CultureInfo, "    <tr><td>{0}</td><td>{1}</td><td>{2:C}</td></tr>", play.Name, perf.Audience,  Convert.ToDecimal(amount / 100)).AppendLine();
+                writer.WriteLine(string.Format(CultureInfo, "<tr><td>{0}</td><td>{1}</td><td>{2:C}</td></tr>", play.Name, perf.Audience, Convert.ToDecimal(amount / 100)));
             }
 
-            result.AppendLine("  </table>");
-            result.AppendFormat(CultureInfo, "  <p>Amount owed is <em>{0:C}</em></p>", Convert.ToDecimal(invoice.TotalAmount / 100)).AppendLine();
-            result.AppendLine($"  <p>You earned <em>{invoice.VolumeCredits}</em> credits</p>");
-            result.Append("</html>");
+            writer.Indent--;
+            writer.WriteLine("</table>");
+            writer.WriteLine(string.Format(CultureInfo, "<p>Amount owed is <em>{0:C}</em></p>", Convert.ToDecimal(invoice.TotalAmount / 100)));
+            writer.WriteLine($"<p>You earned <em>{invoice.VolumeCredits}</em> credits</p>");
+            
+            writer.Indent--;
+            writer.Write("</html>");
 
             return result.ToString();
         }
