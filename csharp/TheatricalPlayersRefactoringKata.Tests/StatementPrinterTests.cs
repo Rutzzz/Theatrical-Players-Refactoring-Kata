e0bbc4716d0f200c3174e7312d1e50a03e91ce5f
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-05-04 19:38:57 +0200

    clear test

--- csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections.Generic;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace TheatricalPlayersRefactoringKata.Tests
 {
@@ -14,9 +13,9 @@ namespace TheatricalPlayersRefactoringKata.Tests
         public void test_statement_plain_text_example()
         {
             Invoice invoice = new Invoice("BigCo", new List<Performance>{
-                new Performance( new TragedyPlay("Hamlet"), 55),
-                new Performance( new ComedyPlay("As You Like It"), 35),
-                new Performance( new TragedyPlay("Othello"), 40)});
+                new( new TragedyPlay("Hamlet"), 55),
+                new( new ComedyPlay("As You Like It"), 35),
+                new( new TragedyPlay("Othello"), 40)});
             
             StatementPrinter statementPrinter = new StatementPrinter();
             var result = statementPrinter.Print(invoice);
@@ -29,9 +28,9 @@ namespace TheatricalPlayersRefactoringKata.Tests
         public void test_statement_html_example()
         {
             Invoice invoice = new Invoice("BigCo", new List<Performance>{
-                new Performance( new TragedyPlay("Hamlet"), 55),
-                new Performance( new ComedyPlay("As You Like It"), 35),
-                new Performance( new TragedyPlay("Othello"), 40)});
+                new( new TragedyPlay("Hamlet"), 55),
+                new( new ComedyPlay("As You Like It"), 35),
+                new( new TragedyPlay("Othello"), 40)});
             
             StatementPrinter statementPrinter = new StatementPrinter();
             
