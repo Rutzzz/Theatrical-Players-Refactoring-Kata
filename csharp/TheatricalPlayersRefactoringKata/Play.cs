2b3231286c0e76e6de8726c44a5d2ca233b9433c
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-05-04 19:37:30 +0200

    clean Play

--- csharp/TheatricalPlayersRefactoringKata/Play.cs
+++ csharp/TheatricalPlayersRefactoringKata/Play.cs
@@ -4,13 +4,12 @@ namespace TheatricalPlayersRefactoringKata
 {
     public abstract class Play
     {
-        private string _name;
+        public string Name { get; set; }
 
-        public string Name { get => _name; set => _name = value; }
-
-        public Play(string name) {
-            this._name = name;
+        protected Play(string name) {
+            Name = name;
         }
+
         public abstract int ComputePrice(int audience);
 
         public virtual int ComputeCredits(int audience)
